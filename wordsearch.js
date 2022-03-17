b8e22ceb37dd9e7b6a02d40d4f6f93907c07f90c
Tanner Farrelly <tannerfarrelly@gmail.com>
2022-03-16 20:46:08 -0400

    fun word search pair program

--- wordsearch.js
+++ wordsearch.js
@@ -2,7 +2,24 @@ const wordSearch = (letters, word) => {
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
+        
     }
-}
+        let arr = [];
+        for (let i = 0; i < letters[0].length; i++){
+          arr.push([]);
+      }
+      for (let a = 0; a < letters.length; a++){
+        for (let b = 0; b < letters[a].length; b++){
+          arr[b][a] = letters[a][b];
+        }
+        
+        const verticalJoin = arr.map(as => as.join(''))
+        for (x of verticalJoin) {
+            if (x.includes(word)) return true
+      }
+    }
+    return false;
+    };
 
+   
 module.exports = wordSearch
